820d018ffd6eacbee78e1fac20a4ff43cdaf083f
Daniel Bimschas <daniel@bimschas.com>
2011-06-29 13:45:39 +0200

    Closed #5 (Implement ForwardingScheduledExecutorService)

--- src/main/java/de/uniluebeck/itm/tr/util/ForwardingScheduledExecutorService.java
+++ src/main/java/de/uniluebeck/itm/tr/util/ForwardingScheduledExecutorService.java
@@ -1,3 +1,26 @@
+/**********************************************************************************************************************
+ * Copyright (c) 2010, Institute of Telematics, University of Luebeck                                                 *
+ * All rights reserved.                                                                                               *
+ *                                                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   *
+ * following conditions are met:                                                                                      *
+ *                                                                                                                    *
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following *
+ *   disclaimer.                                                                                                      *
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the        *
+ *   following disclaimer in the documentation and/or other materials provided with the distribution.                 *
+ * - Neither the name of the University of Luebeck nor the names of its contributors may be used to endorse or promote*
+ *   products derived from this software without specific prior written permission.                                   *
+ *                                                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE      *
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE *
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY   *
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                *
+ **********************************************************************************************************************/
+
 package de.uniluebeck.itm.tr.util;
 
 import com.google.common.collect.Lists;
@@ -6,6 +29,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * A {@link ScheduledExecutorService} implementation that wraps a {@link ScheduledExecutorService} and a {@link
+ * ExecutorService}. The {@link ScheduledExecutorService} instance is only used for scheduling, the actual work is
+ * forwarded to the {@link ExecutorService} instance for immediate execution when the scheduled time arrives.
+ *
+ * @author Daniel Bimschas
+ */
 public class ForwardingScheduledExecutorService implements ScheduledExecutorService {
 
 	private class ForwardingCallable<V> implements Callable<Future<V>> {
@@ -59,13 +89,15 @@ public class ForwardingScheduledExecutorService implements ScheduledExecutorServ
 	@Override
 	public ScheduledFuture<?> scheduleAtFixedRate(final Runnable command, final long initialDelay, final long period,
 												  final TimeUnit unit) {
-		return scheduledExecutorService.scheduleAtFixedRate(new ForwardingRunnable(command), initialDelay, period, unit);
+		return scheduledExecutorService
+				.scheduleAtFixedRate(new ForwardingRunnable(command), initialDelay, period, unit);
 	}
 
 	@Override
 	public ScheduledFuture<?> scheduleWithFixedDelay(final Runnable command, final long initialDelay, final long delay,
 													 final TimeUnit unit) {
-		return scheduledExecutorService.scheduleWithFixedDelay(new ForwardingRunnable(command), initialDelay, delay, unit);
+		return scheduledExecutorService
+				.scheduleWithFixedDelay(new ForwardingRunnable(command), initialDelay, delay, unit);
 	}
 
 	@Override
