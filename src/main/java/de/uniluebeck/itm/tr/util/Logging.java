6fe05443fefc9b31ca7ca4d0520d04633ffc1061
Daniel Bimschas <daniel@bimschas.com>
2011-07-06 13:19:27 +0200

    Closed #6 (Provide means to override default logging)

--- src/main/java/de/uniluebeck/itm/tr/util/Logging.java
+++ src/main/java/de/uniluebeck/itm/tr/util/Logging.java
@@ -23,57 +23,102 @@
 
 package de.uniluebeck.itm.tr.util;
 
+import com.google.classpath.ClassPath;
+import com.google.classpath.ClassPathFactory;
 import org.apache.log4j.*;
-import org.apache.log4j.spi.LoggerRepository;
 
+import java.io.File;
+import java.util.Properties;
 
+/**
+ * Helper class to set logging defaults for log4j in a running JVM.
+ */
+@SuppressWarnings("unused")
 public class Logging {
 
+	/**
+	 * The default {@link PatternLayout}
+	 */
+	private static final String DEFAULT_PATTERN_LAYOUT =
+			"%-23d{yyyy-MM-dd HH:mm:ss,SSS} | %-30.30t | %-30.30c{1} | %-5p | %m%n";
+
+	/**
+	 * Same as calling {@link Logging#setLoggingDefaults(org.apache.log4j.Level, org.apache.log4j.Layout)} with level
+	 * {@link Level#INFO} and layout {@link Logging#DEFAULT_PATTERN_LAYOUT}.
+	 */
 	public static void setLoggingDefaults() {
-
-		// configure logging defaults
-		Appender appender = new ConsoleAppender(
-				new PatternLayout("%-23d{yyyy-MM-dd HH:mm:ss,SSS} | %-25.25t | %-25.25c{1} | %-5p | %m%n")
-		);
-
-		Logger itmLogger = Logger.getLogger("de.uniluebeck.itm");
-		Logger wisebedLogger = Logger.getLogger("eu.wisebed");
-		Logger coaLogger = Logger.getLogger("com.coalesenses");
-		Logger deviceUtilsLogger = Logger.getLogger("de.uniluebeck.itm.wsn.deviceutils");
-		Logger deviceDriversLogger = Logger.getLogger("de.uniluebeck.itm.wsn.drivers");
-
-		if (!itmLogger.getAllAppenders().hasMoreElements()) {
-			itmLogger.addAppender(appender);
-			itmLogger.setLevel(Level.DEBUG);
-		}
-
-		if (!wisebedLogger.getAllAppenders().hasMoreElements()) {
-			wisebedLogger.addAppender(appender);
-			wisebedLogger.setLevel(Level.DEBUG);
-		}
-
-		if (!coaLogger.getAllAppenders().hasMoreElements()) {
-			coaLogger.addAppender(appender);
-			coaLogger.setLevel(Level.INFO);
-		}
-
-		if (!deviceUtilsLogger.getAllAppenders().hasMoreElements()) {
-			deviceUtilsLogger.addAppender(appender);
-			deviceUtilsLogger.setLevel(Level.INFO);
-		}
-
-		if (!deviceDriversLogger.getAllAppenders().hasMoreElements()) {
-			deviceDriversLogger.addAppender(appender);
-			deviceDriversLogger.setLevel(Level.ERROR);
-		}
+		setLoggingDefaults(Level.INFO, new PatternLayout(DEFAULT_PATTERN_LAYOUT));
 	}
 
-
+	/**
+	 * Same as calling {@link Logging#setLoggingDefaults(org.apache.log4j.Level, org.apache.log4j.Layout)} with level
+	 * {@link Level#DEBUG} and layout {@link Logging#DEFAULT_PATTERN_LAYOUT}.
+	 */
 	public static void setDebugLoggingDefaults() {
-		PatternLayout patternLayout = new PatternLayout("%-13d{HH:mm:ss,SSS} | %-20.20c{3} | %-5p | %m%n");
-		final Appender appender = new ConsoleAppender(patternLayout);
-		Logger.getRootLogger().removeAllAppenders();
+		setLoggingDefaults(Level.DEBUG, new PatternLayout(DEFAULT_PATTERN_LAYOUT));
+	}
+
+	/**
+	 * Same as calling {@link Logging#setLoggingDefaults(org.apache.log4j.Level, org.apache.log4j.Layout)} with level
+	 * {@code level} and layout {@link Logging#DEFAULT_PATTERN_LAYOUT}.
+	 *
+	 * @param level the log level to set on the root logger
+	 */
+	public static void setLoggingDefaults(final Level level) {
+		setLoggingDefaults(level, new PatternLayout(DEFAULT_PATTERN_LAYOUT));
+	}
+
+	/**
+	 * Sets the logging defaults according to the following algorithm:
+	 * <p/>
+	 * <ol> <li>If the system property "log4j.configuration" is found and it points to a valid readable file the file is
+	 * passed to log4j as a configuration file.</li> <li>If the first does not apply and a file with the name
+	 * "log4j.properties" is found in the JVMs classpath this file is passed to log4j as a configuration file.</li> <li>If
+	 * neither the first or the second apply log4j is configured to log level {@code level} and layout {@code layout}.</li>
+	 * </ol>
+	 *
+	 * @param level  the log level to set on the root logger
+	 * @param layout the layout to set on the root logger
+	 */
+	public static void setLoggingDefaults(final Level level, final Layout layout) {
+
+		if (System.getProperty("log4j.configuration") != null) {
+
+			File configurationFile = new File(System.getProperty("log4j.configuration"));
+			if (configurationFile.exists() && !configurationFile.isDirectory() && configurationFile.canRead()) {
+				try {
+					PropertyConfigurator.configure(configurationFile.getAbsolutePath());
+					return;
+				} catch (Exception e) {
+					System.err.println(
+							"Tried to load log4j configuration from classpath, resulting in the following exception: {}" + e
+					);
+					System.err.println("Using default logging configuration.");
+				}
+			}
+
+		}
+
+		ClassPathFactory factory = new ClassPathFactory();
+		ClassPath classPath = factory.createFromJVM();
+		if (classPath.isResource("log4j.properties")) {
+			try {
+				Properties properties = new Properties();
+				properties.load(classPath.getResourceAsStream("log4j.properties"));
+				PropertyConfigurator.configure(properties);
+				return;
+			} catch (Exception e) {
+				System.err.println(
+						"Tried to load log4j configuration from classpath, resulting in the following exception: {}" + e
+				);
+				System.err.println("Using default logging configuration.");
+			}
+		}
+
+
+		Appender appender = new ConsoleAppender(layout);
+
 		Logger.getRootLogger().addAppender(appender);
-		Logger.getRootLogger().setLevel(Level.DEBUG);
+		Logger.getRootLogger().setLevel(level);
 	}
 }
